90c41b279d2b48dd13b757fec877f5578a866c0f
[metinoxy] <[metinoz32@gmail.com]>
2024-11-23 00:49:18 +0300

    Numerical Deflection is added

---
+++ MATLAB/Wing_Bending.m
@@ -0,0 +1,61 @@
+clc; clear all; close all;
+% Imported data from xflr5
+wing_y = [-0.815	-0.805	-0.7975	-0.7925	-0.7875	-0.7825	-0.7775	-0.7725	-0.7675	-0.7625	-0.7588	-0.7562	-0.7538	-0.7512	-0.7161	-0.6483	-0.5806	-0.5128	-0.445	-0.3772	-0.3094	-0.2417	-0.1739	-0.137	-0.131	-0.125	-0.119	-0.113	-0.107	-0.101	-0.095	-0.089	-0.083  	0   0.083	0.089	0.095	0.101	0.107	0.113	0.119	0.125	0.131	0.137	0.1739	0.2417	0.3094	0.3772	0.445	0.5128	0.5806	0.6483	0.7161	0.7512	0.7538	0.7562	0.7588	0.7625	0.7675	0.7725	0.7775	0.7825	0.7875	0.7925	0.7975	0.805	0.815];
+M_b_hrz =    [    0 	0.0004	0.0013	0.002	0.003	0.0041	0.0055	0.0071	0.0088	0.0106	0.012	0.0128	0.0137	0.0146	0.0258	0.1201	0.3031	0.5857	0.976	1.48	2.1015	2.8414	3.6968	4.2199	4.3059	4.3927	4.4801	4.5682	4.6569	4.7462	4.836	4.9263	5.0169	5.0169  5.0169	4.9263	4.836	4.7462	4.6569	4.5682	4.4801	4.3927	4.3059	4.2199	3.6968	2.8415	2.1015	1.48	0.976	0.5857	0.3031	0.1201	0.0258	0.0146	0.0137	0.0128	0.012	0.0106	0.0088	0.0071	0.0055	0.0041	0.003	0.002	0.0013	0.0004	0];
+chord =  [ 0.08	    0.09	0.0975	0.1025	0.1075	0.1125	0.1175	0.1225	0.1275	0.1325	0.1362	0.1387	0.1412	0.1437	0.1492	0.1575	0.1658	0.1742	0.1825	0.1908	0.1992	0.2075	0.2158	0.2215	0.2245	0.2275	0.2305	0.2335	0.2365	0.2395	0.2425	0.2455	0.2485	0.2485  0.2485	0.2455	0.2425	0.2395	0.2365	0.2335	0.2305	0.2275	0.2245	0.2215	0.2158	0.2075	0.1992	0.1908	0.1825	0.1742	0.1658	0.1575	0.1492	0.1437	0.1412	0.1387	0.1362	0.1325	0.1275	0.1225	0.1175	0.1125	0.1075	0.1025	0.0975	0.09	0.08];
+
+ro_tube = 2100; % Density of the tube [kg/m^3]
+d_o = 0.016; % Outer diameter of the tube [m]
+d_i = 0.012; % Inner diameter of the tube [m]
+A_tube = pi * (d_o^2 - d_i^2) / 4; % Tube Area [m^2]
+I_tube = pi * (d_o^4 - d_i^4) / 64; % Tube moment of inertia [m^4]
+E_tube = 39e9; % Elastic modulus of GFRP [Pa]
+
+ro_shell = 1264; % Desity of the shell [kg/m^2]
+A_shell = (415.479892e-6/0.25) * chord; % Shell Area [m^2]
+I_shell = (55082.565563e-12/0.25) * chord; % Shell moment of inertia [m^4]
+d_st = 0.003; % Distance btw Shell's Neutral Axis and Tube Neutral Axis [m]
+E_shell = 2.54e9; % Elastic modulus of PLA [Pa]
+
+I_shell_trasformed = I_shell + A_shell*d_st^2; % Transformed I of shell
+
+EI = zeros(0,length(wing_y));
+for i = 1 : length(wing_y)
+    if abs(wing_y(i)) < 0.5
+        EI(i) = E_tube*I_tube + E_shell*I_shell_trasformed(i);
+    else
+        EI(i) = E_shell .* I_shell(i);
+    end
+end
+
+% First integration: Calculate slope
+slope = cumtrapz(wing_y, M_b_hrz ./ EI);
+
+% Adjust slope to enforce boundary condition: slope(0) = 0
+slope = slope - slope(find(wing_y == 0, 1));
+
+% Second integration: Calculate deflection
+deflection = cumtrapz(wing_y, slope);
+
+% Adjust deflection to enforce boundary condition: deflection(0) = 0
+deflection = (deflection - deflection(find(wing_y == 0, 1))).*1000;
+
+% Plot bending moment
+figure;
+subplot(2, 1, 1);
+plot(wing_y, M_b_hrz, 'b-', 'LineWidth', 2);
+grid on;
+xlabel('Spanwise Position [m])');
+ylabel('Bending Moment [Nm]');
+title('Bending Moment Distribution');
+
+% Plot deflection
+subplot(2, 1, 2);
+plot(wing_y, deflection, 'r-', 'LineWidth', 2);
+grid on;
+xlabel('Spanwise Position [m]');
+ylabel('Deflection [mm]');
+title('Deflection Distribution');
+
+
+
